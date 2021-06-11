226530a58d7f2eaf91273b2b92a07f8ea84ea5ae
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2021-06-11 21:21:08 +0900

    Menuを表示できるようにする

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,13 +1,41 @@
 import * as React from 'react';
+import './index.css';
 
 class Menu extends React.Component {
+
+  onClick = async () => {
+    console.log('テストを受ける');
+  }
 
   render() {
     return (
       <div className="menu">
-          <p>
-              Login　Succeeded!
-          </p>          
+        <p>
+          Welcome To Word Test !
+        </p>
+          <button
+            type="button"
+            className="button-like-link"
+            onClick={this.onClick}
+          >
+            テストを受ける
+          </button>
+          <br></br>
+          <button
+            type="button"
+            className="button-like-link"
+            onClick={this.onClick}
+          >
+            過去の結果をみる
+          </button>
+          <br></br>
+          <button
+            type="button"
+            className="button-like-link"
+            onClick={this.onClick}
+          >
+            hoge
+          </button>
       </div>
     );
 
