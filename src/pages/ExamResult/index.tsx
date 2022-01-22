fdbd6df2da7b719d69bc1d67b2f20ea63a548c82
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2022-01-22 23:50:44 +0900

    material-ui導入
    試験結果を表で表す

--- src/pages/ExamResult/index.tsx
+++ src/pages/ExamResult/index.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import LoginStore from '../../core/store/LoginStore';
 import ApiRequestUtil from '../../util/ApiRequestUtil';
 import { ExamResultInfo } from './ExamResultInfo';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 export interface IExamResult {
   loginStore: LoginStore;
@@ -27,16 +32,34 @@ class ExamResult extends React.Component<IExamResult, ExamResultState> {
   }
 
   render() {
-    const listItems = this.state.examResultList?.map((result) => (
-      <div key={result.id}> | {result.score} | {result.userId} | </div> 
-    ));
+    // const listItems = this.state.examResultList?.map((result) => (
+    //   <div key={result.id}> | {result.score} | {result.userId} | </div> 
+    // ));
+    const examResultList = this.state.examResultList;
 
     return (
       <div className="ExamResult">
         <button onClick={this.getExamResultList}>
             取得
         </button>
-        {listItems}
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow >
+                <TableCell>score</TableCell>
+                <TableCell>userId</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {examResultList.map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell>{row.score}</TableCell>
+                  <TableCell>{row.userId}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
 
     );
